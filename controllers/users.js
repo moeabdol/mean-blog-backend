0fe1dbd341445b036ed43cd21c89345d75033027
moeabdol <mohd.a.saed@gmail.com>
2017-09-04 13:11:07 +0300

    Add user model, controller, and routes and implement registration with validation

--- controllers/users.js
+++ controllers/users.js
@@ -3,17 +3,17 @@ const User = require("../models/user");
 const register = (req, res) => {
   if (!req.body.email) {
     return res.status(400).json({
-      message: "Email not provided!"
+      message: "Email must be provided!"
     });
   }
   if (!req.body.username) {
     return res.status(400).json({
-      message: "Username not provided!"
+      message: "Username must be provided!"
     });
   }
   if (!req.body.password) {
     return res.status(400).json({
-      message: "Password not provided!"
+      message: "Password must be provided!"
     });
   }
 
@@ -22,11 +22,56 @@ const register = (req, res) => {
     username: req.body.username.toLowerCase(),
     password: req.body.password
   });
+
   newUser.save((err) => {
-    if (err) return res.status(500).json({
-      message: "Could not create user.",
-      error: err
-    });
+    if (err) {
+      if (err.code === 11000) return res.status(400).json({
+        message: "Username or email already exists!"
+      });
+
+      if (err.errors) {
+        if (err.errors.email) {
+          if (err.errors.email.value === "") return res.status(400).json({
+            message: "Email must not be empty spaces!"
+          });
+          if (err.errors.email.message ===
+            "Email is not a valid email address!") {
+            return res.status(400).json({
+              message: err.errors.email.message
+            });
+          }
+        }
+
+        if (err.errors.username) {
+          if (err.errors.username.value === "") return res.status(400).json({
+            message: "Username must not be empty spaces!"
+          });
+          if (err.errors.username.kind === "minlength") {
+            return res.status(400).json({
+              message: "Username must be at least (3) characters long!"
+            });
+          }
+          if (err.errors.username.kind === "maxlength") {
+            return res.status(400).json({
+              message: "Username must be at most (15) characters long!"
+            });
+          }
+        }
+
+        if (err.errors.password) {
+          if (err.errors.password.kind === "minlength") {
+            return res.status(400).json({
+              message: "Password must be at least (8) characters long!"
+            });
+          }
+        }
+      }
+
+      return res.status(500).json({
+        message: "Internal server error.",
+        error: err
+      });
+    }
 
     res.status(201).json({
       message: "User created successfully."
--- models/user.js
+++ models/user.js
@@ -1,11 +1,57 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const mongoose    = require("mongoose");
+const bcrypt      = require("bcryptjs");
+const Schema      = mongoose.Schema;
+const SALT_FACTOR = 10;
 
 const UserSchema = new Schema({
-  email:    { type: String, required: true, unique: true, lowercase: true },
-  username: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: (email) => {
+        return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
+      }, message: "Email is not a valid email address!"
+    }
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minlength: 3,
+    maxlength: 15
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  }
 });
+
+UserSchema.pre("save", function(next) {
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) return next(err);
+        this.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+UserSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return done(err);
+    done(null, isMatch);
+  });
+};
 
 UserSchema.index({ email: 1, username: 1}, { unique: true });
 
